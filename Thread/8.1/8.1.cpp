512bac8a569cf46b7ec4571f9f37233baa7cf9b5
comedy <chenfangjincomedy@gmail.com>
2020-02-01 13:22:27 +0800

    all

--- Thread/8.1/8.1.cpp
+++ Thread/8.1/8.1.cpp
@@ -1,25 +1,29 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <iostream>
+
+using namespace std;
 
 void *thfunc(void *arg)
 {
-	printf("in thfunc\n");
+	char *parameter =(char*) arg;
+	cout << "hello comedy"<< endl;
 	return (void *)0;
 }
 int main(int argc, char *argv [])
 {
 	pthread_t tidp;
 	int ret;
-	
-	ret = pthread_create(&tidp, NULL, thfunc, NULL);
+	const char *str = "hello comedy";
+	ret = pthread_create(&tidp, NULL, thfunc, (void*)str);
 	if(ret)
 	{
 		 printf("pthread_create failed:%d\n", ret);
 		 return -1;
 	}
 	
-	sleep(1);
+	pthread_join(tidp, NULL);
 	printf("in main: thread is created\n");
 	return 0;
 }
