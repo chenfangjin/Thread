a7a6b2f735db1dc52b0d77a328857f7292cc663c
comedy <chenfangjincomedy@gmail.com>
2020-02-11 20:29:42 +0800

    thread lock

--- Thread/8.9/8.9.cpp
+++ Thread/8.9/8.9.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <list>
+#include <mutex>
 
 using namespace std;
 
@@ -11,8 +12,13 @@ public:
 	{
 		for(int i = 0; i < 1000; i++)
 		{
+			lock(mylock1,mylock2);
+			lock_guard<mutex> myguard1(mylock1,adopt_lock);
+			lock_guard<mutex> myguard2(mylock2,adopt_lock);
+		//	mylock.lock();
 			ticket.push_back(i);
 			cout << "a command is comming" << endl;
+		//	mylock.unlock();
 		}
 	}
 	
@@ -20,15 +26,21 @@ public:
 	{
 		for(int j = 0; j < 1000; j++)
 		{
+			lock(mylock1,mylock2);
+			lock_guard<mutex> myguard1(mylock1,adopt_lock);
+			lock_guard<mutex> myguard2(mylock2,adopt_lock);
+			//	mylock.lock();
 			if(!ticket.empty())
 			{
 				int command = ticket.front();
 				ticket.pop_front();	
 				cout << "command is :" << command << endl;
+			//	mylock.unlock();
 			}
 			else
 			{
 				cout << "there is no people" << endl;	
+			//	mylock.unlock();
 			}
 		}
 		
@@ -36,6 +48,8 @@ public:
 	
 private:
 	list<int> ticket;
+	mutex mylock1;
+	mutex mylock2;
 };
 
 int main(int argc, char *argv[])
